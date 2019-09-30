f189132b48267ee5b9f9b3bc905f956d8ae556ae
Hasimy <ahmadhasyimi674@gmail.com>
2019-09-30 14:41:22 +0700

    Uploading files

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 import express from 'express';
-import mail from './mailer/mailer';
+import bodyParser from 'body-parser';
+import mailer from 'nodemailer';
 
 const server = express();
-const serverName = 'localhost';
-const PORT = 3000 || process.env.PORT;
+const serverType = 'localhost';
+const PORT = 8080 || process.env.port;
 
-server.use('/',  mail);
-server.listen(PORT, () => console.log(`server di ${serverName}:${PORT}!`));
+server.set('view engine', 'ejs');
+server.use(express.static('public'));
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.get('/', (req, res) => res.render('index'));
+server.post('/sendEmail', (req, res) => {
+  let transporter = mailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    auth: {
+      user: 'your email',
+      pass: 'your password'
+    }
+  });
+  let mailOptions = {
+    from: '"achmad_syeh_27rpl@student.smktelkom-mlg.sch.id"', // sender address
+    to: req.body.to, // list of receivers
+    subject: req.body.subject, // subject line
+    text: req.body.body, // plain text body
+    html: '<b>Email Test</b>' // html body
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) throw err;
+    res.render('sent');
+  });
+});
+
+server.listen(PORT, () => console.log(`Connected to ${serverType}:${PORT}`));
